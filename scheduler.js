1908dffccf4a1e94acf4b83e5bfaeeb572b150ca
dnridwn <denridwan123@gmail.com>
2024-08-27 22:23:11 +0700

    fix: undefined variable

--- scheduler.js
+++ scheduler.js
@@ -6,7 +6,7 @@ const { bot } = require('./bot');
 const notion = require('./notion');
 
 // run every minute
-cron.schedule('* * * * *', async () => {
+cron.schedule('* * * * * *', async () => {
     const now = moment().format('YYYY-MM-DD');
     try {
         const date = moment().add(1, 'days').format('YYYY-MM-DD');
@@ -18,7 +18,7 @@ cron.schedule('* * * * *', async () => {
                 const history = await reminderHistory.findByChatIdTaskIdAndSendDate(chat.chat_id, task.id, now);
                 if (!history) {
                     await reminderHistory.create(chat.chat_id, task.id, now);
-                    bot.telegram.sendMessage(chat.chat_id, `You have a task due ${formattedDate}: ${task.properties.Name.title[0].plain_text} (${task.url})`);
+                    bot.telegram.sendMessage(chat.chat_id, `You have a task due ${task.properties['Due Date'].date.start}: ${task.properties.Name.title[0].plain_text} (${task.url})`);
                 }
             });
         });
